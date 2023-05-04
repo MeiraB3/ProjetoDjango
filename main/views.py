a06c8ae190c5302d3ff80ee9cfa3fa06fd2b92d9
MeiraB3 <gabrielhmeira13@gmail.com>
2023-05-04 01:16:41 -0300

    commit0.9
    continuação do desenvolvimento django

--- main/views.py
+++ main/views.py
@@ -1,9 +1,14 @@
 from .models import Aluno
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def alunoView(request):
     alunos_list = Aluno.objects.all()
     return render(request,'main/alunos.html',{'alunos_list':alunos_list})
 
+
+def alunoIDview(request):
+    aluno = get_object_or_404(Aluno, pk=id)
+    print(aluno)
+    return render(request, 'main/alunoID.html',{'aluno':aluno})
