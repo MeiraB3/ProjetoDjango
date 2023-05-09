19c5ab303eca082a17b585ed0e6781aad98320f5
MeiraB3 <gabrielhmeira13@gmail.com>
2023-05-08 21:28:29 -0300

    forms_django
    criação web de um forms através do Django.

--- main/urls.py
+++ main/urls.py
@@ -3,16 +3,10 @@ from . import views
 from .views import AlunoCreateView, AlunoUpdateView
 
 urlpatterns = [
-    path('', views.alunoView, name='aluno-lista'),
-    #################################
-    path('alunoID/<int:id>', views.alunoIDview, name='aluno-detalhe'),
-    #################################
-    path('contact', views.contact_view, name='aluno-contact'),
-    #################################
-    path('aluno/create/', AlunoCreateView.as_view(), name='aluno-create'),
-    path('aluno/<int:pk>/update/', AlunoUpdateView.as_view(), name='aluno-update'),
-    path('delete/<int:id>', views.deleteAluno, name="delete-aluno"),
-
+    path('',views.alunoView, name='aluno-lista'),
+    path('alunoID/<int:id>', views.alunoIDview, name = 'aluno-detalhe'),
+    path('contact', views.contact_View, name='aluno-contact'),
+    path('aluno/create/', AlunoCreateView.as_view(), name = 'aluno-create'),
+    path('aluno/<int:pk>/update/', AlunoUpdateView.as_view(), name = 'aluno-update'),
+    
 ]
-
-
